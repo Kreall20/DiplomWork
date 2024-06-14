c7e5cdecbee7b43899664187718b3c2f0a6908a8
Kreall20 <ivzenkokirill@gmail.com>
2024-06-14 15:14:27 +0600

    viewTeeth

--- React/src/pages/MedicalCard/TothCardPage.jsx
+++ React/src/pages/MedicalCard/TothCardPage.jsx
@@ -13,30 +13,30 @@ const handleToothClick = (toothNumber) => {
 };
 
 const ToothCardPage = () => {
-    const createToothButtons = () => {
-        const toothOrder = [8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5, 6, 7, 8];
+    const createToothButtons = (toothOrder, isTopRow) => {
         return toothOrder.map((number, index) => (
-            <button className="ToothBlock" onClick={() => handleToothClick(number)} key={index}>
+            <button
+                className={`ToothBlock ${isTopRow ? `top-row-${number}` : `bottom-row-${number}`}`}
+                onClick={() => handleToothClick(number)}
+                key={index}
+            >
                 <img src={ToothImage} alt={`Зуб ${number}`} className="ToothImage" />
                 <span className="ToothNumber">{number}</span>
             </button>
         ));
     };
 
+    const topRowOrder = [8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5, 6, 7, 8];
+    const bottomRowOrder = [8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5, 6, 7, 8];
+
     return (
         <main>
             <div className="headername">
                 <h1>Карточка Пациента</h1>
             </div>
-            {/* <form className="Searchform" action="">
-                <label htmlFor="searchPatient"></label>
-                <div className="searchblock">
-                    <input className="Search" id="searchPatient" type="text" placeholder="Поиск" />
-                </div>
-            </form> */}
             <div className="TethList">
-                <div className="ToothRow">{createToothButtons()}</div>
-                <div className="ToothRow">{createToothButtons()}</div>
+                <div className="ToothRow">{createToothButtons(topRowOrder, true)}</div>
+                <div className="ToothRow">{createToothButtons(bottomRowOrder, false)}</div>
             </div>
             <div className="TothButtons">
                 <Link to="/medicalCards/card/add-record"><button className="TothButton" onClick={scrollToTop}>Добавить запись</button></Link>
